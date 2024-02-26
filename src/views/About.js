b48be0f9a793462b9100e1340e8209926ec71a25
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-02-26 11:13:45 -0500

    Creando rutas

---
+++ src/views/About.js
@@ -0,0 +1,30 @@
+export const renderItems = (data) => {
+    // Aquí comienza tu código y puedes retornar lo que tu necesites
+    //Crear Funciones a usar
+    let root = "";
+    data.forEach((movies) => {
+      root +=  `
+      <div class = "wrapper" bis_skin_checked="1">
+       <div class="item-container" bis_skin_checked="1">
+         <div class="movie-info" bis_skin_checked="1">
+            <div class = "imagen" bis_skin_checked="1">
+             <dd itemprop = "imagen" class="ddimagen">
+             <img class = "img_movies" src=${movies.imageUrl}>
+              </dd>
+            </div>
+           <div class = "atras">
+           <dl itemscope itemtype="MOVIES">
+           <dt>Nombre:</dt><dd itemprop="name">${movies.name}</dd>
+           <dt>Short descripción:</dt><dd itemprop="shortdescription">${movies.shortDescription}</dd>
+           <dt>Premios Ganados:</dt><dd itemprop="datos_importantes">${movies.facts.premioGanado}</dd>
+           </dl></div>
+         </div>
+        </div>
+      </div>`
+  
+    });
+   
+    // Retornar el contenedor actualizado
+    return root;
+    
+  };
