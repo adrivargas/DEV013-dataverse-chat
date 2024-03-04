b8480998de95ac6e97234ad74ff9a961af20e311
Gabriela Ramos <gabyr.contact@gmail.com>
2024-03-04 09:53:54 -0500

    probando cambios

--- src/views/About.js
+++
@@ -1,6 +0,0 @@
-export function About(props) {
-  const viewEl = document.createElement('div');
-  viewEl.classList.add('about');
-  viewEl.textContent = 'This is the About page.';
-  return viewEl;
-}
