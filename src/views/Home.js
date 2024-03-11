591166de7f157ef6187239220fad19b530a8dcc4
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-03-11 11:13:32 -0500

    Actualizando mi codigo

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,15 @@
 //linkEl.addEventListener('click', () => navigateTo("/about", { name: "Xochitl" }))
 import { header } from '../components/header.js';
+import data from '../data/dataset.js';
+import { renderData } from '../components/RenderData.js';
 import { footer } from '../components/footer.js';
-import data from '../data/dataset.js'
-import { renderData } from '../components/renderData.js';
 //renderdata en componente no en datafuction
 export function Home(props) {
     const viewEl = document.createElement('div');
     const movieData = document.createElement("div");   // contenedor de cartas (o elementos HTML) que se generarán dinámicamente.
     movieData.className = "cartasContainer";
-    movieData.innerHTML = renderData(data)
-    viewEl.appendChild(header());
-    viewEl.appendChild(movieData);
-    viewEl.appendChild(footer());
-
+    movieData.innerHTML = renderData(data) 
+    viewEl.append(header(),movieData,footer());
     return viewEl;
 }
+
