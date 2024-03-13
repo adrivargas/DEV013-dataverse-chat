7a684afbff4a4578218b4b55fc8684edd6f37d93
Gabriela Ramos <gabyr.contact@gmail.com>
2024-03-12 20:11:16 -0500

    vistas listas  home, bienvenida, Error

--- src/views/Bienvenida.js
+++ src/views/Bienvenida.js
@@ -21,24 +21,4 @@ export const Bienvenida = (props) => {
   return viewEl;
   } 
     
-      //const viewBtn = document.createElement('div');
-    //viewEl.classList.add('home-container');
-
-    // Crear el elemento del encabezado (header)
-    //const pTitele = document.createElement('p');
-    //const btnStart = document.createElement('button');
-
-     
-    //pTitele.textContent = 'Bienvenida!';
-   // viewBtn.appendChild(btnStart);
-
-    //const customHeader= header();
-    //headerEl.appendChild(customHeader);
-
-    // Agregar el encabezado al div
  
-   //viewEl.append(header(), pTitele, viewBtn, footer());
-
-   // return viewEl;
-
-//}
