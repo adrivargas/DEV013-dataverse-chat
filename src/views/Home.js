310b317ee9569578ba8d330bb4d3a27f3a75ead5
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-03-14 11:53:03 -0500

    Home

--- src/views/Home.js
+++ src/views/Home.js
@@ -22,9 +22,11 @@ export function Home({ id }) {
     //const selectSort = document.querySelector('[data-testid="select-sort"]');
     
     const selectFilter = document.querySelector('[data-testid="select-filter"]');
-    const selectSort = filtersElement.getElementByid('[data-testid="select-filter"]');
+    const selectSort = filtersElement.getElementsByid("#categorias");
+
     console.log(filtersElement);
     console.log(selectSort);
+  
     // selectSort.addEventListener('click',(event)=>{
     //     console.log("valw");
 
